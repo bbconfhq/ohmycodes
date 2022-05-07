5d76b1b83db61678bc478203c6883e63571a1bae
Ryo <9062624+gwanryo@users.noreply.github.com>
2022-05-07 16:50:18 +0900

    feat: validate title max length 191

--- server/models/code.go
+++ server/models/code.go
@@ -7,7 +7,7 @@ import (
 type Code struct {
 	ID        string    `gorm:"primaryKey"                 json:"id"`
 	Ip        string    `gorm:"not null"                   json:"ip"      validate:"required,ip4_addr"`
-	Title     string    `gorm:"type:varchar(191);not null" json:"title"   validate:"required"`
+	Title     string    `gorm:"type:varchar(191);not null" json:"title"   validate:"required,max=191"`
 	Content   string    `gorm:"not null"                   json:"content" validate:"required"`
 	CreatedAt time.Time `gorm:"not null"                   json:"created_at"`
 	ExpiredAt time.Time `gorm:"not null"                   json:"expired_at"`
