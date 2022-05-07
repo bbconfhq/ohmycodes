0334542dd6fbbd71c6c64f7db98c5c2c7103a6f8
Ryo <9062624+gwanryo@users.noreply.github.com>
2022-05-08 00:40:21 +0900

    fix: set ip default to 0.0.0.0

--- server/models/code.go
+++ server/models/code.go
@@ -6,7 +6,7 @@ import (
 
 type Code struct {
 	ID        string    `gorm:"primaryKey"                                 json:"id"`
-	Ip        string    `gorm:"not null"                                   json:"ip"`
+	Ip        string    `gorm:"not null;default:'0.0.0.0'"                 json:"ip"`
 	Name      string    `gorm:"type:varchar(191);not null;default:'user'"  json:"name"    validate:"required,max=191"`
 	Title     string    `gorm:"type:varchar(191);not null;default:'title'" json:"title"   validate:"required,max=191"`
 	Content   string    `gorm:"not null"                                   json:"content" validate:"required"`
