9ec32465a54829325518e2ba7aa879d1a4d44b1a
blurfx <iam@xo.dev>
2023-05-07 21:04:00 +0900

    feat: line highlighting

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,50 +1,50 @@
 <script lang="ts">
-	import { afterUpdate } from 'svelte';
-	import DarkMode from 'svelte-dark-mode';
+  import { afterUpdate } from 'svelte';
+  import DarkMode from 'svelte-dark-mode';
 
-	let theme;
+  let theme;
 
-	afterUpdate(() => {
-	    document.body.className = theme;
-	});
+  afterUpdate(() => {
+    document.body.className = theme;
+  });
 
-	import '../app.css';
+  import '../app.css';
 </script>
 
 <DarkMode bind:theme />
 
 <main id="root">
-	<header>
-		<h1 id="title"><a href="/">ohmy.codes</a></h1>
-	</header>
-	<div id="content">
-		<slot />
-	</div>
+  <header>
+    <h1 id="title"><a href="/">ohmy.codes</a></h1>
+  </header>
+  <div id="content">
+    <slot />
+  </div>
 </main>
 
 <style lang="scss">
-	#root {
-		min-height: 100%;
-		padding: 2rem;
+  #root {
+    min-height: 100%;
+    padding: 2rem;
 
-		background: radial-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.15));
+    background: radial-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.15));
 
-		header {
-			h1 {
-				text-align: center;
-				margin-bottom: 2.5rem;
+    header {
+      h1 {
+        text-align: center;
+        margin-bottom: 2.5rem;
 
-				a {
-					color: rgba(255, 255, 255, 0.7);
-					text-decoration: none;
-					letter-spacing: 4px;
-				}
-			}
-		}
+        a {
+          color: rgba(255, 255, 255, 0.7);
+          text-decoration: none;
+          letter-spacing: 4px;
+        }
+      }
+    }
 
-		#content {
-			max-width: 100%;
-			margin: 0 auto;
-		}
-	}
+    #content {
+      max-width: 100%;
+      margin: 0 auto;
+    }
+  }
 </style>
---
+++ frontend/src/utils/dom.ts
@@ -0,0 +1,47 @@
+import {setHash} from "./navigator";
+
+export const findNearestLineNumberElement = (el: Element): Element | null => {
+  const lineNumberEls = document.querySelectorAll('.line-number');
+  let nearestEl: Element | null  = null;
+  let minDistance = Infinity;
+
+  const binarySearchNearest = (arr, value, start, end) => {
+    if (start > end) {
+      return null;
+    }
+
+    const mid = Math.floor((start + end) / 2);
+    const distance = Math.abs(arr[mid].offsetTop - value.offsetTop);
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      nearestEl = arr[mid];
+    }
+
+    if (arr[mid].offsetTop > value.offsetTop) {
+      return binarySearchNearest(arr, value, start, mid - 1);
+    } else if (arr[mid].offsetTop < value.offsetTop) {
+      return binarySearchNearest(arr, value, mid + 1, end);
+    } else {
+      return arr[mid];
+    }
+  };
+
+  binarySearchNearest(Array.from(lineNumberEls), el, 0, lineNumberEls.length - 1);
+
+  return nearestEl;
+};
+
+export const highlightLines = (startElement, endElement, className) => {
+  let currentElement = startElement;
+  while (currentElement && currentElement !== endElement.nextSibling) {
+    if (currentElement.nodeType === Node.ELEMENT_NODE) {
+      currentElement.classList.add(className);
+    }
+    currentElement = currentElement.nextSibling;
+  }
+  const start = startElement.getAttribute('data-line');
+  const end = endElement.getAttribute('data-line');
+  setHash({ start, end });
+};
+
