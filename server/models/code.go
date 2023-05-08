5c9c12a978f4d32ab16fc28a4a8e8736851baf92
Ryo <9062624+gwanryo@users.noreply.github.com>
2023-05-09 01:13:54 +0900

    fix: remove language default value

--- server/models/code.go
+++ server/models/code.go
@@ -10,7 +10,7 @@ type Code struct {
 	Name      string    `gorm:"type:varchar(191);not null"  json:"name"     validate:"required;max=191"`
 	Title     string    `gorm:"type:varchar(191);not null"  json:"title"    validate:"required;max=191"`
 	Content   string    `gorm:"not null"                    json:"content"  validate:"required"`
-	Language  string    `gorm:"not null;default:'auto'"     json:"language" validate:"required;max=191"`
+	Language  string    `gorm:"not null"                    json:"language" validate:"required;max=191"`
 	CreatedAt time.Time `gorm:"not null"                    json:"created_at"`
 	ExpiredAt time.Time `gorm:"not null"                    json:"expired_at"`
 }
