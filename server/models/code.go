76e27fd842eca1f3dced455d3f591809ac5f8dba
Ryo <9062624+gwanryo@users.noreply.github.com>
2023-05-09 01:18:14 +0900

    fix: typo

--- server/models/code.go
+++ server/models/code.go
@@ -7,10 +7,10 @@ import (
 type Code struct {
 	ID        string    `gorm:"primaryKey"                  json:"id"`
 	Ip        string    `gorm:"not null;default:'0.0.0.0'"  json:"ip"`
-	Name      string    `gorm:"type:varchar(191);not null"  json:"name"     validate:"required;max=191"`
-	Title     string    `gorm:"type:varchar(191);not null"  json:"title"    validate:"required;max=191"`
+	Name      string    `gorm:"type:varchar(191);not null"  json:"name"     validate:"required,max=191"`
+	Title     string    `gorm:"type:varchar(191);not null"  json:"title"    validate:"required,max=191"`
 	Content   string    `gorm:"not null"                    json:"content"  validate:"required"`
-	Language  string    `gorm:"not null"                    json:"language" validate:"required;max=191"`
+	Language  string    `gorm:"not null"                    json:"language" validate:"required,max=191"`
 	CreatedAt time.Time `gorm:"not null"                    json:"created_at"`
 	ExpiredAt time.Time `gorm:"not null"                    json:"expired_at"`
 }
